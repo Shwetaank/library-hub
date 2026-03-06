479b447fd66295d93692264e55bce5c93af89d83
Shwetaank <spmorey87@gmail.com>
2026-03-07 02:10:59 +0530

    prisma error fixed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "../generated/prisma";
 import { PrismaMssql } from "@prisma/adapter-mssql";
 
 const globalForPrisma = globalThis as unknown as {
